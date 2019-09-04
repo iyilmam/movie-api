415d6577d496f34055407927475f40f6963fc5ba
ilker YILMAM <ilkeryilmam@gmail.com>
2019-09-04 13:07:52 +0300

    get movies with director

--- routes/movie.js
+++ routes/movie.js
@@ -6,7 +6,25 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 
 router.get('/', (req, res) => {
-  const promise = Movie.find({});
+  const promise = Movie.aggregate([
+    {
+      $lookup: {
+        from: 'directors',
+        localField: 'director_id',
+        foreignField: '_id',
+        as: 'director'
+      }
+    },
+    {
+      $unwind: {
+        path: '$director',
+        preserveNullAndEmptyArrays: true
+      }
+
+    }
+  ]);
+
+
   promise.then((data) => {
     res.json(data);
   }).catch((err) => {
